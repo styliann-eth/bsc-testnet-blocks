8154b01748871e341744a064eb9566eb407d8a9f
styliann-eth <ns2808@protonmail.com>
2023-02-04 19:15:55 +0000

    BSC Testnet changes

---
+++ sample-queries/index.js
@@ -0,0 +1,32 @@
+import { request, gql } from 'graphql-request'
+
+// This query returns the first 5 blocks of the first 10 minutes of 2020.
+const fastQuery = gql`
+  {
+    blocks(first: 5, orderBy: timestamp, orderDirection: asc,
+        where: {timestamp_gt: "1577836800", timestamp_lt:"1577837400"}) {
+      id
+      number
+      timestamp
+      parentHash
+    }
+  }
+`
+
+// Note that the id field that gets returned is the hash of the block.
+const query = gql`
+  {
+    blocks(first: 1, skip:0, orderBy: timestamp, orderDirection: desc) {
+      id
+      number
+      timestamp
+      author
+      difficulty
+      gasUsed
+      gasLimit
+      parentHash
+    }
+  }
+`
+
+request('https://api.thegraph.com/subgraphs/name/matthewlilley/bsc-blocks', query).then((data) => console.log(data))
